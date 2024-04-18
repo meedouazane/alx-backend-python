1aae0361c912447e160ffd45e3b963f517f2b8d5
Ouazane <mohamedouazane@yahoo.com>
2024-04-18 04:26:30 -0700

    3-to_str.py

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+""" Basic annotations - to_str """
+
+
+def to_str(n: float) -> str:
+    """ function to_str that takes float and returns their string """
+    return str(n)
