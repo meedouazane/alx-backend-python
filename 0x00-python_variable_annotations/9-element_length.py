e4f3d335375a2bef26632a5b19210c29464e8aec
Ouazane <mohamedouazane@yahoo.com>
2024-04-18 05:15:06 -0700

    9-element_length.py

--- 0x00-python_variable_annotations/9-element_length.py
+++ 0x00-python_variable_annotations/9-element_length.py
@@ -1,10 +1,9 @@
 #!/usr/bin/env python3
 """ Let's duck type an iterable object """
 import typing
-from typing import Tuple
 
 
 def element_length(lst: typing.Iterable[typing.Sequence]) ->\
-        Tuple[typing.Sequence, int]:
+        typing.List[typing.Tuple[typing.Sequence, int]]:
     """function’s parameters and return values with the appropriate types"""
     return [(i, len(i)) for i in lst]
