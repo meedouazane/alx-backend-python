b11b6d218cbbce909d3aa1ec3a51113c786d15bb
Ouazane <mohamedouazane@yahoo.com>
2024-04-22 05:55:55 -0700

    1-concurrent_coroutines.py

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -5,7 +5,7 @@ import asyncio
 wait_random = __import__('0-basic_async_syntax').wait_random
 
 
-async def wait_n(n, max_delay):
+async def wait_n(n: int, max_delay: int) -> list:
     """return the list of all the delays """
     lst = []
     for _ in range(n):
