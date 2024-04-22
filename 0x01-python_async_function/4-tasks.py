3d5781f548fb39bddb5caed6fec186e5058842fc
Ouazane <mohamedouazane@yahoo.com>
2024-04-22 06:18:03 -0700

    4-tasks.py

--- 0x01-python_async_function/4-tasks.py
+++ 0x01-python_async_function/4-tasks.py
@@ -1,11 +1,12 @@
 #!/usr/bin/env python3
 """Tasks"""
-import typing
+from typing import List
+
 wait_random = __import__('0-basic_async_syntax').wait_random
 task_wait_random = __import__('3-tasks').task_wait_random
 
 
-async def task_wait_n(n: int, max_delay: int) -> typing.List[float]:
+async def task_wait_n(n: int, max_delay: int) -> List[float]:
     """return the list of all the delays """
     lst = []
     for _ in range(n):
