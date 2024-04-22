6926177210b74858758df949344be2367b762782
Ouazane <mohamedouazane@yahoo.com>
2024-04-22 05:21:34 -0700

    1-concurrent_coroutines.py

---
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""Let's execute multiple coroutines at the same time with async"""
+import asyncio
+
+wait_random = __import__('0-basic_async_syntax').wait_random
+
+
+async def wait_n(n, max_delay):
+    """return the list of all the delays """
+    lst = []
+    for _ in range(n):
+        delay = await wait_random(max_delay)
+        lst.append(delay)
+        lst.sort()
+    return lst
