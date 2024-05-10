c5877fc713912d6fecafa16285ed349e7ccf2af9
Ouazane <mohamedouazane@yahoo.com>
2024-05-10 04:52:03 -0700

    test_utils.py

---
+++ 0x03-Unittests_and_integration_tests/README.md
@@ -0,0 +1,3 @@
+0x03. Unittests and Integration Tests
+The difference between unit and integration tests.
+Common testing patterns such as mocking, parametrizations and fixtures
---
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+""" Parameterize a unit test """
+import unittest
+from utils import access_nested_map
+from parameterized import parameterized
+
+
+class TestAccessNestedMap(unittest.TestCase):
+    """ test that the method returns what it is supposed to """
+
+    @parameterized.expand([
+        ({"a": 1}, ('a'), 1),
+        ({"a": {"b": 2}}, ('a'), {"b": 2}),
+        ({"a": {"b": 2}}, ('a', 'b'), 2)
+    ])
+    def test_nested(self, nested_map, path, expected):
+        self.assertEquals(access_nested_map((nested_map), path), expected)
+
+
+if __name__ == '__main__':
+    unittest.main()
